4bbfc86be2a4e9308c97e41c14603151821653d1
webiovg <kevin.kowalsky1@gmail.com>
2022-08-01 18:18:25 +0200

    fourth type of enemy added

--- game.js
+++ game.js
@@ -131,13 +131,13 @@ let enemiesArray = [
     },
     {
         name: 'enemy3',
-        numberOfEnemies: 5,
+        numberOfEnemies: 1,
         numberOfFramesInSpritesheet: 6,
         enemiesArray: []
     },
     {
         name: 'enemy4',
-        numberOfEnemies: 0,
+        numberOfEnemies: 1,
         numberOfFramesInSpritesheet: 9,
         enemiesArray: []
     },
@@ -242,6 +242,46 @@ class Enemy3 {
     }
 }
 
+class Enemy4 {
+    constructor() {
+        this.image = new Image();
+        this.image.src = 'img/enemies/enemy4.png';
+        this.speed = Math.random() * 4 + 1;
+        this.spriteWidth = 213;
+        this.spriteHeight = 212;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height - 150);
+        this.newX = Math.random() * (canvas.width - this.width);
+        this.newY = Math.random() * (canvas.height - this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+        this.interval = Math.round(Math.random() * 200 + 50);
+    }
+
+    update() {
+        if (gameFrame % this.interval === 0) {
+            this.newX = Math.random() * (canvas.width - this.width);
+            this.newY = Math.random() * (canvas.height - this.height);    
+        }
+        let dx = this.x - this.newX;
+        let dy = this.y - this.newY;
+        this.x -= dx/70;
+        this.y -= dy/70;
+
+        if (this.x + this.width < 0) this.x = canvas.width;
+
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > enemiesArray[1].numberOfFramesInSpritesheet - 2 ? this.frame = 0 : this.frame++;
+        }
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+}
+
 // Fill enemiesArray with all enemies instances
 enemiesArray.forEach(enemy => {
     for (i = 0; i < enemy.numberOfEnemies; i++) {
@@ -249,7 +289,7 @@ enemiesArray.forEach(enemy => {
             case 'enemy1': enemy.enemiesArray.push(new Enemy1()); break;
             case 'enemy2': enemy.enemiesArray.push(new Enemy2()); break;
             case 'enemy3': enemy.enemiesArray.push(new Enemy3()); break;
-            // case 'enemy4': enemy.enemiesArray.push(new Enemy4()); break;
+            case 'enemy4': enemy.enemiesArray.push(new Enemy4()); break;
             default: console.log('Error during the filling of enemiesArray variable (enemies instanciations)'); break;
         }
     }

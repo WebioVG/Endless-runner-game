ee611368c1900d8a54cb3484384451c04190ae24
webiovg <kevin.kowalsky1@gmail.com>
2022-08-05 01:04:43 +0200

    game width increased

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ window.addEventListener('load', () => {
     
     const canvas = document.querySelector('#canvas1');
     const ctx = canvas.getContext('2d');
-    const CANVAS_WIDTH = canvas.width = 600;
+    const CANVAS_WIDTH = canvas.width = 1000;
     const CANVAS_HEIGHT = canvas.height = 600;
     canvas.style.width = CANVAS_WIDTH.toString() + 'px';
     

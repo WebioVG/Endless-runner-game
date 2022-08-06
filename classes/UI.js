ba854585b75d25bffb9aee789f272278ece162df
webiovg <kevin.kowalsky1@gmail.com>
2022-08-06 18:56:02 +0200

    UI class code made cleaner

--- classes/UI.js
+++ classes/UI.js
@@ -8,7 +8,22 @@ export class UI {
     draw() {
         this.game.ctx.save();
 
-        // General UI settings
+        this.setGeneralSettings();
+        this.drawCurrentScore();
+        this.drawTimer();
+        this.drawLives();
+        this.drawEnergy();
+        this.drawWinningScore();
+        this.drawEventMessage();
+        if (this.game.gameOver) this.drawGameOver();
+
+        this.game.ctx.restore();
+    }
+
+    /**
+     * Sets the general settings of the UI.
+     */
+    setGeneralSettings() {
         this.game.ctx.shadowOffsetX = 2;
         this.game.ctx.shadowOffsetY = 2;
         this.game.ctx.shadowColor = 'white';
@@ -16,48 +31,48 @@ export class UI {
         this.game.ctx.font = `${this.fontSize}px ${this.fontFamily}`;
         this.game.ctx.textAlign = 'left';
         this.game.ctx.fillStyle = this.game.fontColor;
+    }
 
-        // Current score
+    drawCurrentScore() {
         this.game.ctx.fillText(`Score: ${this.game.score}`, 20, 50);
+    }
 
-        // Timer
+    drawLives() {
+        for (let i = 0; i < this.game.player.lives; i++) this.game.ctx.drawImage(lives, 25 * i + 20, 95, 25, 25);
+    }
+
+    drawTimer() {
         this.game.ctx.font = `${this.fontSize * 0.8}px ${this.fontFamily}`;
         this.game.ctx.fillText(`Time: ${(this.game.time * 0.001).toFixed(1)}s`, 20, 80);
+    }
 
-        // Lives
-        for (let i = 0; i < this.game.player.lives; i++) {
-            this.game.ctx.drawImage(lives, 25 * i + 20, 95, 25, 25);
-        }
-
-        // Energy
+    drawEnergy() {
         this.game.ctx.font = `${this.fontSize * 0.8}px ${this.fontFamily}`;
         this.game.ctx.fillText(`Energy: ${this.game.player.energy}`, 20, 150);
-        
-        // Winning score
+    }
+
+    drawWinningScore() {
         this.game.ctx.fillText(`Winning score: ${this.game.winningScore}`, this.game.width - 200, 50);
-        
-        // Event message
-        // if (this.game.event !== null) console.log(this.game.eventTimer);
+    }
+
+    drawEventMessage() {
         this.game.ctx.font = `${this.fontSize * 1.2}px ${this.fontFamily}`;
         if (this.game.event !== null && this.game.eventTimer <= this.game.event.duration) this.game.ctx.fillText(`${this.game.event.messageUI}`, 200, 80);
+    }
 
-        // Game Over
-        if (this.game.gameOver) {
-            // Game over general settings
-            this.game.ctx.textAlign = 'center';
-            this.game.ctx.font = `${this.fontSize * 2}px ${this.fontFamily}`;
-            
-            // Game over message
-            if (this.game.score >= this.game.winningScore) {
-                this.game.ctx.fillText('You won!', this.game.width * 0.5, this.game.height * 0.5 - 20);
-                this.game.ctx.font = `${this.fontSize * 0.7}px ${this.fontFamily}`;
-                this.game.ctx.fillText('Night creatures went home', this.game.width * 0.5, this.game.height * 0.5 + 20);
-                this.game.ctx.fillText(`after you eliminated ${this.game.score} of them!`, this.game.width * 0.5, this.game.height * 0.5 + 45);
-            } else {
-                this.game.ctx.fillText('Game over', this.game.width * 0.5, this.game.height * 0.5);
-            }
+    drawGameOver() {
+        // Game over general settings
+        this.game.ctx.textAlign = 'center';
+        this.game.ctx.font = `${this.fontSize * 2}px ${this.fontFamily}`;
+        
+        // Game over message
+        if (this.game.score >= this.game.winningScore) {
+            this.game.ctx.fillText('You won!', this.game.width * 0.5, this.game.height * 0.5 - 20);
+            this.game.ctx.font = `${this.fontSize * 0.7}px ${this.fontFamily}`;
+            this.game.ctx.fillText('Night creatures went home', this.game.width * 0.5, this.game.height * 0.5 + 20);
+            this.game.ctx.fillText(`after you eliminated ${this.game.score} of them!`, this.game.width * 0.5, this.game.height * 0.5 + 45);
+        } else {
+            this.game.ctx.fillText('Game over', this.game.width * 0.5, this.game.height * 0.5);
         }
-
-        this.game.ctx.restore();
     }
 }
